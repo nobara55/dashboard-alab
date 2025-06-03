05e13a9a7d5be9d0c80fc502b78896388972c10b
nobara55 <gil5@tutamail.com>
2025-06-02 20:18:57 -0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
-from app import app
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
